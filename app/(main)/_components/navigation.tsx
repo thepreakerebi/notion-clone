8de16c0b72378d7449861af611eb8a492775a4b7
Elvis Kerebi <kerebipreye@gmail.com>
2025-03-30 18:05:06 +0200

    added sidebar toggle button

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,7 +1,30 @@
 "use client";
 
+import { ChevronsLeft } from "lucide-react";
+
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <>
+      <aside className="group/sidebar h-full bg-secondary overflow-y-auto relative w-60 flex-col z-[99999]">
+        <div
+          role="button"
+          className="h-6 w-6 text-muted-foreground rounded-sm
+           hover:bg-neutral-300 dark:hover:bg-neutral-600 
+           absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 
+           transition"
+        >
+          <ChevronsLeft className="h-6 w-6" />
+        </div>
+        <div>
+          <p>Action items</p>
+        </div>
+        <div className="mt-4">
+          <p>Documents</p>
+        </div>
+        <div className="opacity-0 group-hover/sidebar:opaciy-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
+      </aside>
+    </>
+  );
 };
 
 export default Navigation;
