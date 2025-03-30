404540bf56bf8d3b80edb58aef8e236b5e6deaa2
Elvis Kerebi <kerebipreye@gmail.com>
2025-03-30 02:26:12 +0200

    set up favicon and header for marketing page

---
+++ app/(marketing)/_components/header.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+const Header = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Your Ideas, Documents, & Plans. Unified. Welcome to{" "}
+        <span className="underline">Jotion</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,11 @@
+import Header from "./_components/header";
+
+export default function MarketingPage() {
+  return (
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Header />
+      </div>
+    </div>
+  );
+}
