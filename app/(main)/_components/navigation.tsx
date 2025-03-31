a8182fdd6502a648051b825e2e1d41f3dcc37def
Elvis Kerebi <kerebipreye@gmail.com>
2025-04-01 00:56:17 +0200

    search command setup done

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -25,8 +25,10 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import TrashBox from "./trash-box";
+import { useSearch } from "@/hooks/use-search";
 
 const Navigation = () => {
+  const search = useSearch();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const isResizingRef = useRef(false);
@@ -152,7 +154,7 @@ const Navigation = () => {
         </div>
         <div>
           <UserItem />
-          <Item label="Search" icon={Search} isSearch onClick={handleCreate} />
+          <Item label="Search" icon={Search} isSearch onClick={search.onOpen} />
           <Item label="Settings" icon={Settings} onClick={handleCreate} />
           <Item onClick={handleCreate} label="New document" icon={PlusCircle} />
         </div>
