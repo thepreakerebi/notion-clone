1f6128c8e6ca26a17cbe27326abcbd84bc129630
Elvis Kerebi <kerebipreye@gmail.com>
2025-03-31 02:48:46 +0200

    added create document function to frontend

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -4,9 +4,29 @@ import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 const DocumentsPage = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = async () => {
+    try {
+      const promise = create({
+        title: "Untitled",
+      });
+
+      await toast.promise(promise, {
+        loading: "Creating a new note...",
+        success: "New note created!",
+        error: "Failed to create new note.",
+      });
+    } catch (error) {
+      console.error("Failed to create note:", error);
+    }
+  };
 
   return (
     <div className="h-full flex flex-col justify-center items-center space-y-4">
@@ -27,7 +47,7 @@ const DocumentsPage = () => {
       <h2 className="text-lg font-medium">
         Welcome to {user?.firstName}&apos;s Jotion!
       </h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className="mr-2 h-4 w-4" /> Create a note
       </Button>
     </div>
