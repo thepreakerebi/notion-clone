8b083d0d5dafcdd6f9350e690e8a0110874ee51b
Elvis Kerebi <kerebipreye@gmail.com>
2025-04-02 18:14:34 +0200

    setup emoji and cover image adding on a document

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,5 +1,36 @@
-const DocumentIdPage = () => {
-  return <div>Document Id Page</div>;
+"use client";
+
+import Toolbar from "@/components/toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
+
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getByID, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    return <div>Loading...</div>;
+  }
+  if (document === null) {
+    return <div>Document not found</div>;
+  }
+
+  return (
+    <div className="pb-40 ">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
 };
 
 export default DocumentIdPage;
